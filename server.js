05a25af3a386ff5f05f3565e328e9ba0592ccc57
Mddolar Islam <imddolar85@gmail.com>
2026-09-17 18:53:41 +0300

    Enable CORS for LiveKit token server

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const { AccessToken } = require("livekit-server-sdk");
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.json({
